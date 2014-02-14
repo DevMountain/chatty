dade7d6a2ff5c7e61db2b1bef0a12d340c305b16
MathewHickman <mhickman01@gmail.com>
2014-02-13 19:57:52 -0700

    Broken Commit

--- app/scripts/controllers/message.js
+++ app/scripts/controllers/message.js
@@ -5,4 +5,15 @@ angular.module('chattyApp')
     $scope.messages = [];
   });
   .controller('MessageCtrl', function ($scope, $http) {
+  	console.log('MessageCtrl')
+    $http.get("http://localhost:11900/").success(function(messages) {
+    	$scope.messages = messages;	
+    });
+    $score.addMessage = function() {
+		var message = {message: $scope.myNewMessageText};
+		$http.post("http://localhost:11900", myNewMessage).success(function() {
+			$scope.messages.push(myNewMessage);
+
+		});
+	}
 });
