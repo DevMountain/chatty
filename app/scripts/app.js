fa728b1030ec57f69a4bb2d34ac85e55aa7add9b
cahlan <cahlan@gmail.com>
2014-06-11 17:37:05 -0600

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,16 @@
 'use strict';
 
-angular.module('chattyApp', []);
+var app = angular.module('chattyApp', []);
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
