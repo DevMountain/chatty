8a007eeba1fe2cf276b4ba6b0ca1f054c1ef9955
cahlan <cahlan@gmail.com>
2014-10-14 20:47:48 -0600

    Update messageservice.js

--- scripts/services/messageservice.js
+++ scripts/services/messageservice.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('chattyApp')
-  .service('MessageService', function MessageService() {
+  .factory('MessageService', function MessageService() {
     return {
     }
   });
