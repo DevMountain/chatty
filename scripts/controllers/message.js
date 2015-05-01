ec69986b98c6599247abd2c6c623ced01578e89b
Rick Cid <rick@hispand.com>
2015-04-30 22:06:18 -0600

    Added Service and Controller to Chatty

--- scripts/controllers/message.js
+++ scripts/controllers/message.js
@@ -1,6 +1,16 @@
-'use strict';
+var app = angular.module('chattyApp');
 
-angular.module('chattyApp')
-  .controller('MessageCtrl', function ($scope) {
-    $scope.messages = [];
+app.controller('MessageCtrl', function($scope, MessageService) {
+  $scope.messages = MessageService.getMessages().then(function(data) {
+    console.log('MessageCtrl: data', data);
+    $scope.messages = data;
   });
+
+  $scope.addMessage = function() {
+    MessageService.addMessage($scope.newMessage).then(function(data) {
+      $scope.messages = data;
+      $scope.newMessage = '';
+    });
+  };
+
+});
